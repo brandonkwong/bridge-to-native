0f7e376662ab7d044733422fab3ecfc723cf036f
Brandon Kwong <brandon@brandonkwong.com>
2018-03-15 16:13:02 -0700

    Setup simple ios swift app with tab bar for Native and React Native

---
+++ ios/Tabby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tabby
+//
+//  Created by Brandon Kwong on 3/15/18.
+//  Copyright © 2018 Brandon Kwong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
